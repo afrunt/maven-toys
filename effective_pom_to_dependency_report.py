31cc525006fe45c8e1b59ff56c8112a354ae2a10
Andrii Frunt <andrii.frunt@gmail.com>
2021-05-13 01:47:31 +0300

    init

--- effective_pom_to_dependency_report.py
+++ effective_pom_to_dependency_report.py
@@ -1,10 +1,16 @@
 """ This script converts effective pom.xml to dependency report in JSON format """
-
 import xml.etree.ElementTree as ET
+
+import jsonpickle
 
 EXIT_CODE_OK, EXIT_CODE_ERROR = 0, 1
 
 XML_NS_POM = "http://maven.apache.org/POM/4.0.0"
+
+
+class DependencyReport:
+    def __init__(self):
+        pass
 
 
 class Project:
@@ -13,8 +19,11 @@ class Project:
 
 
 class Dependency:
-    def __init__(self):
-        pass
+    def __init__(self, group_id, artifact_id, version, scope):
+        self.group_id = group_id
+        self.artifact_id = artifact_id
+        self.version = version
+        self.scope = scope
 
 
 def pom_ns_tag_name(tag):
@@ -30,26 +39,42 @@ def child_tag_to(parent, tag, func, default):
     return func(element) if element is not None else default
 
 
+def child_tags_text_to_tuple(parent, *args):
+    return tuple([child_tag_to_text(parent, tag) for tag in args])
+
+
 def xml_element_to_dependency(element):
-    dependency = Dependency()
-    dependency.group_id = child_tag_to_text(element, "groupId")
-    dependency.artifact_id = child_tag_to_text(element, "artifactId")
-    dependency.version = child_tag_to_text(element, "version")
-    dependency.scope = child_tag_to_text(element, "scope")
+    g, a, v, s = child_tags_text_to_tuple(element, "groupId", "artifactId", "version", "scope")
+    dependency = Dependency(g, a, v, s)
     return dependency
 
 
 def xml_element_to_project(element):
     project = Project()
-    project.group_id = child_tag_to_text(element, "groupId")
-    project.artifact_id = child_tag_to_text(element, "artifactId")
-    project.version = child_tag_to_text(element, "version")
-    project.packaging = child_tag_to_text(element, "packaging")
+    values = child_tags_text_to_tuple(element, "groupId", "artifactId", "version", "packaging")
+    project.group_id = values[0]
+    project.artifact_id = values[1]
+    project.version = values[2]
+    project.packaging = values[3]
 
-    project.dependencies = child_tag_to(element, "dependencies", lambda deps: [xml_element_to_dependency(d) for d in deps], [])
+    def dependencies_from_elements(dependencies_element):
+        return [xml_element_to_dependency(d) for d in dependencies_element]
+
+    project.dependencies = child_tag_to(element, "dependencies", dependencies_from_elements, [])
 
     return project
 
 
 def parse_effective_pom_xml(file_path):
     return [xml_element_to_project(element) for element in ET.parse(file_path).getroot()]
+
+
+def create_dependency_report(file_path):
+    projects = parse_effective_pom_xml(file_path)
+    report = DependencyReport()
+    report.projects = projects
+    return report
+
+
+def create_dependency_report_json(file_path):
+    return jsonpickle.encode(create_dependency_report(file_path), indent=True)
--- test_effective_pom_to_dependency_report.py
+++ test_effective_pom_to_dependency_report.py
@@ -2,10 +2,12 @@ import unittest
 
 import effective_pom_to_dependency_report as ef
 
+SAMPLE_EFFECTIVE_POM_XML = "sample-data/effective-pom.xml"
+
 
 class DependencyReportTest(unittest.TestCase):
     def test_parse_effective_pom_xml(self):
-        projects = ef.parse_effective_pom_xml("sample-data/effective-pom.xml")
+        projects = ef.parse_effective_pom_xml(SAMPLE_EFFECTIVE_POM_XML)
         self.assertIsNotNone(projects)
         self.assertEqual(len(projects), 3)
         self.assertEqual(projects[0].packaging, "pom")
@@ -14,6 +16,23 @@ class DependencyReportTest(unittest.TestCase):
         self.assertEqual(projects[0].version, "1.0-SNAPSHOT")
         self.assertIsNone(projects[1].packaging)
         self.assertIsNone(projects[2].packaging)
+
+        self.assertIsNotNone(projects[1].dependencies)
+        self.assertEqual(len(projects[1].dependencies), 1)
+        self.assertEqual(projects[1].dependencies[0].artifact_id, "flying-saucer-pdf")
+        self.assertEqual(projects[1].dependencies[0].version, "9.1.20")
+        self.assertEqual(projects[1].dependencies[0].scope, "compile")
+
+        self.assertIsNotNone(projects[2].dependencies)
+        self.assertEqual(len(projects[2].dependencies), 1)
+        self.assertEqual(projects[2].dependencies[0].artifact_id, "flying-saucer-pdf")
+        self.assertEqual(projects[2].dependencies[0].version, "9.1.22")
+        self.assertEqual(projects[2].dependencies[0].scope, "compile")
+
+    def test_create_dependency_report_json(self):
+        json = ef.create_dependency_report_json(SAMPLE_EFFECTIVE_POM_XML)
+        print(json)
+        self.assertIsNotNone(json)
 
 
 if __name__ == '__main__':
